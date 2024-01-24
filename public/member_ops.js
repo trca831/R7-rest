33417e35c0db818669eb807b674a53907b5c567e
Tracy Cano <tracycano3.14@gmail.com>
2024-01-23 22:11:33 -0800

    calling REST with Fetch

--- public/member_ops.js
+++ public/member_ops.js
@@ -80,7 +80,7 @@ function handle_members(event) {
               alert(
                 `Return code ${response.status} ${
                   response.statusText
-                } ${JSON.stringify(data)}`,
+                } ${JSON.stringify(data)}`
               );
             })
             .catch((error) => {
@@ -118,7 +118,7 @@ function handle_members(event) {
               alert(
                 `Return code ${response.status} ${
                   response.statusText
-                } ${JSON.stringify(data)}`,
+                } ${JSON.stringify(data)}`
               );
             })
             .catch((error) => {
@@ -143,7 +143,7 @@ function handle_members(event) {
               alert(
                 `Return code ${response.status} ${
                   response.statusText
-                } ${JSON.stringify(data)}`,
+                } ${JSON.stringify(data)}`
               );
             })
             .catch((error) => {
@@ -153,9 +153,18 @@ function handle_members(event) {
         }
       });
     } else if (event.target === deleteMember) {
-    // Your code goes here!
-    } else if (event.target === listFacts) {
-      fetch(`${members_path}/${memberId3.value}/facts`).then((response) => {
+    
+      // Your code goes here!
+      let headers = { "Content-Type": "application/json" };
+      let csrf_cookie = getCookie("CSRF-TOKEN");
+      if (csrf_cookie) {
+        headers["X-CSRF-Token"] = csrf_cookie;
+      }
+      fetch(`${members_path}/${memberId2.value}`,
+      {
+        method: "DELETE",
+        headers: headers,
+      }).then((response) => {
         if (response.status === 200) {
           response.json().then((data) => {
             resultsDiv.innerHTML = "";
@@ -179,10 +188,127 @@ function handle_members(event) {
             });
         }
       });
+
+    } else if (event.target === listFacts) {
+      fetch(`${members_path}/${memberId3.value}/facts`).then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            resultsDiv.innerHTML = "";
+            let parag = document.createElement("P");
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+        } else {
+          response
+            .json()
+            .then((data) => {
+              alert(
+                `Return code ${response.status} ${
+                  response.statusText
+                } ${JSON.stringify(data)}`
+              );
+            })
+            .catch((error) => {
+              console.log(error);
+              alert(error);
+            });
+        }
+      });
     } else if (event.target === createFact) {
       // Your code goes here!
+      // var dataObject = {
+      //   member_id: memberId4.value,
+      //   fact_text: factText.value,
+      //   likes: likes.value,
+      // };
+
+      let headers = { "Content-Type": "application/json" };
+      let csrf_cookie = getCookie("CSRF-TOKEN");
+      if (csrf_cookie) {
+        headers["X-CSRF-Token"] = csrf_cookie;
+      }
+      let dataObject = {
+        fact_text: factText.value,
+        likes: likes.value,
+      };
+      fetch(`${members_path}/${memberId4.value}/facts`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(dataObject),
+      }).then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            resultsDiv.innerHTML = "";
+            let parag = document.createElement("P");
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+        } else {
+          response
+            .json()
+            .then((data) => {
+              alert(
+                `Return code ${response.status} ${
+                  response.statusText
+                } ${JSON.stringify(data)}`,
+              );
+            })
+            .catch((error) => {
+              console.log(error);
+              alert(error);
+            });
+        }
+      });
+    
     } else if (event.target === updateFact) {
       // Your code goes here!
+      // var dataObject = {
+      //   member_id: memberId5.value,
+      //   fact_text: factText2.value,
+      //   likes: likes2.value,
+      // };
+      let headers = { "Content-Type": "application/json" };
+      let csrf_cookie = getCookie("CSRF-TOKEN");
+      if (csrf_cookie) {
+        headers["X-CSRF-Token"] = csrf_cookie;
+      }
+      let dataObject = {
+        member_id: memberId5.value,
+        fact_text: factText2.value,
+        likes: likes2.value,
+      };
+      fetch(`${members_path}/${memberId5.value}/facts/${factNumber.value}`, {
+        method: "PATCH",
+        headers,
+        body: JSON.stringify(dataObject)
+      }).then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            resultsDiv.innerHTML = "";
+            let parag = document.createElement("P");
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+        } else {
+          response
+            .json()
+            .then((data) => {
+              alert(
+                `Return code ${response.status} ${
+                  response.statusText
+                } ${JSON.stringify(data)}`,
+              );
+            })
+            .catch((error) => {
+              console.log(error);
+              alert(error);
+            });
+        }
+      });
+    
+  
+
+
     } else if (event.target === showFact) {
       fetch(`${members_path}/${memberId6.value}/facts/${factNumber2.value}`).then(
         (response) => {
